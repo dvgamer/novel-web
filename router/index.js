d278b17b6c15b79117c61ec6495042e855403352
Kananek T <info.dvgamer@gmail.com>
2017-04-16 16:30:53 +0700

    create

---
+++ router/index.js
@@ -0,0 +1,41 @@
+"use strict";
+const express = require('express');
+const router = express.Router();
+const mongoose 	= require('mongoose');
+const request = require('request-promise');
+const MONGODB = process.env.MONGODB;
+
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+router.use(cors());
+router.use(bodyParser.urlencoded({ extended: false }))
+router.use(bodyParser.json())
+
+mongoose.connect(`mongodb://${MONGODB?MONGODB:'127.0.0.1:27017'}/db_kingkan`);
+mongoose.Promise = global.Promise;
+
+router.get('/search', (req, res) => {
+  request({
+    url: 'https://www.xn--12c1c1aaf8feo.com/search/box',
+    method: 'post',
+    headers: {
+    	'Host': 'www.xn--12c1c1aaf8feo.com',
+      'Accept': 'text/html, */*; q=0.01',
+      'X-Requested-With': 'XMLHttpRequest',
+      'Content-Type': 'application/x-www-form-urlencoded'
+    },
+    data: {
+      q: '9789742361556',
+      ajaxxxx: true,
+      ajaxxxx_dataType: 'html'
+    }
+  }).then(res => {
+  	res.end(JSON.stringify({ hrml: res }))
+  }).catch(err => {
+  	console.log('err', err)
+  	res.end()
+  })
+})
+
+module.exports = router;
---
+++ src/App.vue
@@ -0,0 +1,70 @@
+<template>
+  <div id="app">
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#">นิยายของฉัน</a>
+            </div>
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+                    <li>
+                        <a href="#">หน้าแรก</a>
+                    </li>
+                    <li>
+                        <a href="#">รายการทั้งหมด</a>
+                    </li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container -->
+    </nav>
+
+    <!-- Page Content -->
+    <div class="container">
+      <router-view></router-view>
+      <!-- /.row -->
+      <hr>
+      <!-- Footer -->
+      <footer>
+        <div class="row">
+          <div class="col-lg-12">
+            <p>Copyright &copy; Your Website 2014</p>
+          </div>
+          <!-- /.col-lg-12 -->
+        </div>
+        <!-- /.row -->
+      </footer>
+
+    </div>
+  </div>
+</template>
+
+<script>
+window.$ = require('jquery')
+window.jQuery = window.$
+
+export default {
+  name: 'app'
+}
+</script>
+
+<style>
+body {
+    padding-top: 70px; /* Required padding for .navbar-fixed-top. Remove if using .navbar-static-top. Change if height of navigation changes. */
+}
+
+footer {
+    margin: 50px 0;
+}
+</style>
